937fb0a19da519c8231cec797b0f43eada11a0e4
a1093342 <100509587+a1093342@users.noreply.github.com>
2023-02-25 23:08:10 +0800

    Add files via upload

---
+++ A1093342_0224_1.java
@@ -0,0 +1,10 @@
+public class A1093342_0224_1 {
+    public static void main(String [] argv){
+        System.out.println("() What is the answer of 20 >> 1 + 1 << 1 * 3");
+        System.out.println("a.40\tb.60\tc.30\td.18");
+
+        int i = 20 >> 1 + 1 << 1 * 3;
+        System.out.println("answer is a."+i);
+
+    }
+}
---
+++ A1093342_0224_2.java
@@ -0,0 +1,13 @@
+public class A1093342_0224_2 {
+    public static void main(String[] argv){
+        System.out.println("What is the value of i when: ");
+        System.out.println("int i = 10 ");
+        System.out.println("i += 1.34 ");
+        System.out.println("a.1.34\tb.11\tc.11.34\td.error");
+
+        int i = 10;
+        i += 1.34;
+
+        System.out.println("The answer is b."+i);
+    }
+}
